282e1cfd8cd57719ba164a3153a992e735c558c1
zhangfengzan1 <zhangfengzan@aichiptech.com>
2021-06-09 11:59:12 +0800

    init zlg kernel branch
    Signed-off-by: zhangfengzan1 <zhangfengzan@aichiptech.com>

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 3 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#3 PREEMPT Wed Jun 9 10:44:16 CST 2021"
+#define LINUX_COMPILE_TIME "10:44:16"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "zfz-virtual-machine"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.4 (4.4.4_09.06.2010) "
